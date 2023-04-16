ffb8f37e05656bc5107486c3239b24a02adccef7
Christian Hosam (1599578) <chosam@berkeley.edu>
2023-04-15 18:41:10 -0700

    Preliminary Work on Black Women's Insurance Landscape - API Call to pull in IPUMS Microdata

--- 02_r_scripts/Insurance_Coverage_EDA.R
+++ 02_r_scripts/Insurance_Coverage_EDA.R
@@ -1,9 +1,37 @@
 #Library Import
+if (!require("ipumsr")) stop("Reading IPUMS data into R requires the ipumsr package. It can be installed using the following command: install.packages('ipumsr')")
 library(tidyverse)
 library(devtools)
-library(congressData)
 library(readxl)
 library(readr)
+if (!require(remotes)) install.packages("remotes")
+remotes::install_github("ipums/ipumsr/ipumsexamples")
+remotes::install_github(
+  "ipums/ipumsr", 
+  build_vignettes = TRUE, 
+  dependencies = TRUE
+)
 
-#Import Dataset 
-Basic_HealthInsurance <- read_csv("00_raw_data/ACSST1Y2021.S2701-2023-03-25T063835.csv")
+#API Call To Import Data
+set_ipums_api_key("59cba10d8a5da536fc06b59dba6936aacdfc4183a2ec23d67211303a")
+
+extract_definition <- define_extract_usa(
+  "This is an example extract to submit via API.",
+  c("us2018a","us2019a"),
+  c("AGE", "SEX", "RACE", "STATEFIP")
+)
+
+submitted_extract <- submit_extract(extract_definition)
+submitted_extract <- get_extract_info(submitted_extract)
+is_extract_ready(submitted_extract)
+downloadable_extract <- wait_for_extract(submitted_extract)
+
+path_to_ddi_file <- download_extract(downloadable_extract)
+data <- read_ipums_micro(path_to_ddi_file)
+
+#Attempted to Import Dataset Manually - Wouldn't Work for Some Reason
+#ddi <- read_ipums_ddi("00_raw_data/usa_00002.xml")
+#data <- read_ipums_micro("00_raw_data/usa_00002.xml")
+
+
+
