f4d143de728463dd6ab6b4db2420316a75e67f2d
Christian Hosam (1599578) <chosam@berkeley.edu>
2023-04-17 17:46:00 -0700

    Cleaned and subsetted data; started table on health insurance coverage of BW by type

--- 02_r_scripts/Insurance_Coverage_EDA.R
+++ 02_r_scripts/Insurance_Coverage_EDA.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(devtools)
 library(readxl)
 library(readr)
+library(kableExtra)
 if (!require(remotes)) install.packages("remotes")
 remotes::install_github("ipums/ipumsr/ipumsexamples")
 remotes::install_github(
@@ -16,9 +17,10 @@ remotes::install_github(
 set_ipums_api_key("59cba10d8a5da536fc06b59dba6936aacdfc4183a2ec23d67211303a")
 
 extract_definition <- define_extract_usa(
-  "This is an example extract to submit via API.",
-  c("us2018a","us2019a"),
-  c("AGE", "SEX", "RACE", "STATEFIP")
+  "Insurance Information by Race and Gender",
+  c("us2021c"),
+  c("AGE", "SEX", "RACE", "STATEFIP", "HCOVANY", "HCOVPRIV", "HINSEMP", "HINSPUR", "HINSTRI", "HCOVPUB", "HINSCAID", "HINSCARE", "HINSVA", "HINSIHS", "INCTOT", "FTOTINC",
+    "INCWAGE", "CITIZEN", "EDUC", "EMPSTAT", "CBPERNUM", "CBSERIAL", "PERWT", "SAMPLE")
 )
 
 submitted_extract <- submit_extract(extract_definition)
@@ -26,12 +28,39 @@ submitted_extract <- get_extract_info(submitted_extract)
 is_extract_ready(submitted_extract)
 downloadable_extract <- wait_for_extract(submitted_extract)
 
-path_to_ddi_file <- download_extract(downloadable_extract)
-data <- read_ipums_micro(path_to_ddi_file)
+ddi <- read_ipums_ddi("00_raw_data/usa_00004.xml")
+data <- read_ipums_micro(ddi)
+ipums_view(data)
 
-#Attempted to Import Dataset Manually - Wouldn't Work for Some Reason
-#ddi <- read_ipums_ddi("00_raw_data/usa_00002.xml")
-#data <- read_ipums_micro("00_raw_data/usa_00002.xml")
+# Clean Variables for EDA
+censususadata <- data
+censususadata <- censususadata %>% 
+  mutate(RACE_factor = as_factor(RACE))
+censususadata <- censususadata %>%
+  mutate(GENDER_factor = as_factor(SEX))
+
+#Create subset of data limited to Black Women
+BWsubset <- censususadata %>%
+  filter(RACE_factor == "Black/African American" & GENDER_factor == "Female")
+
+#Mutate Variables Again as a Check
+censususadata <- censususadata %>%
+  mutate(RACE_zapped = zap_labels(RACE))
+censususadata <- censususadata %>%
+  mutate(GENDER_zapped = zap_labels(SEX))
+BWsubset2 <- censususadata %>%
+  filter(RACE_zapped == "2" & GENDER_zapped == "2")
+
+#Zap Labels from Health Insurance Variables
+BWSubset3 <- BWsubset %>%
+  mutate_at(vars(starts_with("HC") | starts_with("HI")), zap_labels)
+
+#Create Frequency Table of Health Insurance Coverage by Type 
+table1 <- BWSubset3 %>%
+  across(select(HCOVANY:HINSIHS), count = freq())
+  
 
 
+table2 <- table1 %>%
+  summarise(freq = n())
 
